1806ecfc788004b0295b36ca4cf84e284d72c25d
Marisa Triola <marisatriola@gmail.com>
2021-10-24 17:58:41 -0400

    add everything up to error handling

---
+++ index.js
@@ -0,0 +1,101 @@
+const { Client, MessageEmbed, Intents } = require('discord.js');
+const axios = require('axios')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { parseRoll, parseSpell, parseItem } = require('./messageparsing.js');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async (message) => {
+    try {
+        if (message.content.startsWith("!roll")) {
+
+            let command = parseRoll(message.content.trim().replace('!roll', ''));
+            let total = roll(command)
+            let send = {
+                title: `${command.ndice}d${command.die}`,
+                description: total,
+                color: 0xD7C363
+            }
+            if (command.mod) send.title += (command.mod < 0 ? " - " : " + ") + command.mod
+
+            await message.channel.send({ embed: send });
+        }
+        else if (message.content.startsWith('!spell')) {
+            let command = await parseSpell(message.content.trim().replace('!spell', ''));
+            let total = roll(command)
+            let send = {
+                title: `${command.spell}: ${command.ndice}d${command.die}`,
+                description: total,
+                color: 0xD7C363
+            }
+            if (command.mod) send.title += (command.mod < 0 ? " - " : " + ") + command.mod
+
+            await message.channel.send({ embed: send });
+        }
+
+        else if (message.content.startsWith('!item')) {
+            let command = await parseItem(message.content.trim().replace('!item', ''));
+
+            let total = roll(command)
+            let send = {
+                title: `${command.item}: ${command.ndice}d${command.die}`,
+                description: total,
+                color: 0xD7C363
+            }
+            if (command.mod) send.title += (command.mod < 0 ? " - " : " + ") + command.mod
+
+            await message.channel.send({ embed: send });
+        }
+    } catch (e) {
+        if (e instanceof NotFoundError) {
+            let send = {
+                title: `Error`,
+                description: e.message,
+                color: 0xff6666
+            }
+            await message.channel.send({ embed: send });
+        }
+        else { console.log(e) }
+    }
+})
+
+
+roll = command => {
+    total = 0
+    for (i = 0; i < command.ndice; i++) {
+        total += Math.ceil(Math.random() * command.die);
+    }
+    total += command.mod;
+    return total;
+}
+
+getSpell = async spell => {
+    try {
+        res = await axios.get(`https://www.dnd5eapi.co/api/spells/${spell}`)
+        return res.data;
+    }
+    catch {
+        throw new NotFoundError("Spell not found")
+    }
+}
+getItem = async item => {
+    try {
+        res = await axios.get(`https://www.dnd5eapi.co/api/equipment/${item}`)
+        return res.data;
+    }
+    catch {
+        throw new NotFoundError("Item not found")
+    }
+}
+
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message); // (1)
+        this.name = "NotFoundError"; // (2)
+    }
+}
+
+
+client.login('ODE5NjYzMTUzNzA4MTM4NTk3.YEp4zA.8Yformhhnv2TqPd8ttxPEXo67EE');
---
+++ messageparsing.js
@@ -0,0 +1,70 @@
+module.exports.parseRoll = msg => {
+    let command = {}
+
+    command.mod = getMod(msg);
+    msg = msg.split('+')[0]
+    msg = msg.split('-')[0]
+    dice = getDice(msg)
+    command.ndice = dice.ndice
+    command.die = dice.die
+
+    return command;
+}
+
+module.exports.parseSpell = async msg => {
+    let command = {}
+
+    command.mod = getMod(msg);
+    msg = msg.split('+')[0]
+    msg = msg.split('-')[0]
+
+    command.spell = msg.trim().toLowerCase().replace(" ", "-")
+    spell = await getSpell(command.spell);
+    spellDice = getDice(spell.damage.damage_at_slot_level[spell.level]);
+    command.ndice = spellDice.ndice
+    command.die = spellDice.die
+
+    return command;
+}
+
+module.exports.parseItem = async msg => {
+    let command = {}
+
+    command.mod = getMod(msg);
+    msg = msg.split('+')[0]
+    msg = msg.split('-')[0]
+
+    command.item = msg.trim().toLowerCase().replace(" ", "-")
+    item = await getItem(command.item);
+    itemDice = getDice(item.damage.damage_dice);
+    command.ndice = itemDice.ndice
+    command.die = itemDice.die
+
+    return command;
+}
+
+getDice = msg => {
+    dice = {}
+    if (msg.includes('d')) {
+        dice.ndice = parseInt(msg.split('d')[0]);
+        dice.die = parseInt(msg.split('d')[1]);
+        if (!dice.ndice) {
+            dice.ndice = 1;
+        }
+    }
+    return dice
+
+}
+
+getMod = msg => {
+    if (msg.includes('+')) {
+        mod = parseInt(msg.split('+')[1]);
+    }
+    else if (msg.includes('-')) {
+        mod = -1 * msg.split('-')[1];
+    }
+    else {
+        mod = 0;
+    }
+    return mod
+}
